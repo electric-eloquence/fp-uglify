7b7eec683112b7e452f054862e25de48c32a5bf0
e2tha-e <ee@electric-eloquence.io>
2018-08-06 12:17:41 -0400

    dependency updates

--- uglify~extend.js
+++ uglify~extend.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const fs = require('fs-extra');
 const gulp = require('gulp');
-const plugins = require('gulp-load-plugins')();
 const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
 
-const appDir = global.appDir;
 const conf = global.conf;
 
 const jsBldDir = conf.ui.paths.source.jsBld;
@@ -13,7 +11,7 @@ const jsSrcDir = conf.ui.paths.source.jsSrc;
 
 gulp.task('uglify', function () {
   return gulp.src(jsSrcDir + '/**/*.js')
-    .pipe(plugins.uglify())
+    .pipe(uglify())
     .pipe(rename({extname: '.min.js'}))
     .pipe(gulp.dest(jsBldDir));
 });
